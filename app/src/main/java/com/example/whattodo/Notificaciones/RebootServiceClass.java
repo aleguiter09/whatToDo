a0747763c7c6cc67c9371b1c4a6819d1ae74b8c3
fio154 <51134863+fio154@users.noreply.github.com>
2021-02-21 16:13:01 -0300

    implementando notificaciones

---
+++ app/src/main/java/com/example/whattodo/Notificaciones/RebootServiceClass.java
@@ -0,0 +1,33 @@
+package com.example.whattodo;
+import android.app.IntentService;
+import android.app.Notification;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+public class RebootServiceClass extends IntentService {
+
+    public RebootServiceClass(String name) {
+        super(name);
+        startForeground(1, new Notification());
+    }
+
+    public RebootServiceClass() {
+        super("RebootServiceClass");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String intentType = intent.getExtras().getString("caller");
+        if (intentType == null) return;
+        if (intentType.equals("RebootReceiver")) {
+            SharedPreferences settings = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+            Utils.setAlarm(settings.getInt("alarmID", 0), settings.getLong("alarmTime", 0), this);
+        }
+    }
+}
